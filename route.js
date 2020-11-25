f096b073c6b9ecec3dc0adae5242713e30ce437d
Iyanu Akins <sundax4u2@gmail.com>
2020-11-25 09:32:00 +0100

    fixed default url route bug

--- route.js
+++ route.js
@@ -4,34 +4,34 @@ const blockchainService = require("./Blockchain/Web");
 
 module.exports = http.createServer((req, res) => {
     const reqUrl = url.parse(req.url, true);
+    
+    let resp = {
+        "success": false,
+        "data": null
+    }
 
     if(reqUrl.pathname.toLocaleLowerCase() === "/getlogs" && req.method === "GET"){
         res.statusCode = 200;
         res.setHeader("content-Type", "Application/json");
         const service = new blockchainService();
-        let res = {
+        resp = {
             "success": true,
             "data": service.transactionLogs
         }
-        res.end(JSON.stringify(res));
+        res.end(JSON.stringify(resp));
     }
     else if(reqUrl.pathname.toLocaleLowerCase() === "/getgroupedlogs" && req.method === "GET"){
         res.statusCode = 200;
         res.setHeader("content-Type", "Application/json");
         const service = new blockchainService();
-        let res = {
+        resp = {
             "success": true,
             "data": service.groupedTansactionLogs
         }
-        res.end(JSON.stringify(res));
+        return res.end(JSON.stringify(resp));
     }
-    else {
-        res.statusCode = 404;
-        res.setHeader("content-Type", "Application/json");
-        let res = {
-            "success": false,
-            "data": null
-        }
-        res.end(JSON.stringify(res));
-    }
+
+    res.statusCode = 404;
+    res.setHeader("content-Type", "Application/json");
+    res.end(JSON.stringify(resp));
 });
